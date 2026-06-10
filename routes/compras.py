1e68f44adb8d2abef81a5398a24272768ba9b471
abvielma <abvielma777@gmai.com>
2026-06-10 12:01:54 -0500

    Compras

--- routes/compras.py
+++ routes/compras.py
@@ -1,13 +1,11 @@
 from flask import Blueprint, render_template, jsonify, request, session, send_file, make_response, Response
 from psycopg2.extras import RealDictCursor, DictCursor
 from database import (
-    obtener_ordenes_recientes, 
-    crear_orden_compra_transaccional, 
+    obtener_ordenes_recientes,
+    crear_orden_compra_transaccional,
     obtener_orden_completa,  # ← Nombre correcto
     db_query, db_execute, db_tx, get_connection,
-    buscar_proveedor_por_ruc, 
-    buscar_proveedores_mejorado
-)
+    buscar_proveedor_por_ruc)
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle, Spacer
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
