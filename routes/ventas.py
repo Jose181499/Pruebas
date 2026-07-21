c7f837594455d7f1513d549979685b759953d9ad
abvielma <abvielma777@gmai.com>
2026-07-21 11:41:52 -0500

    AR

--- routes/ventas.py
+++ routes/ventas.py
@@ -12,7 +12,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'usuario' not in session:
-            return redirect(url_for('auth.login'))
+           return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decorated_function
 
